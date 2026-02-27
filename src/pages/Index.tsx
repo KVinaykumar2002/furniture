df3ee4e0d54e06749cfd03444c528f0541068428
MedamoniSharan <sharan.medamoni1729@gmail.com>
2026-02-27 18:55:35 +0530

    removed category duplicate

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,6 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/home/HeroSection";
 import ShopByCategory from "@/components/home/ShopByCategory";
-import CategoryBanners from "@/components/home/CategoryBanners";
 import CompletedProjectsAndShowrooms from "@/components/home/CompletedProjectsAndShowrooms";
 import WhyChooseUs from "@/components/home/WhyChooseUs";
 import PromoStrip from "@/components/home/PromoStrip";
@@ -17,7 +16,6 @@ export default function Index() {
       <Navbar />
       <HeroSection />
       <ShopByCategory />
-      <CategoryBanners />
       <CompletedProjectsAndShowrooms />
       <WhyChooseUs />
       <PromoStrip />
